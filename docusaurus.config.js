b6cf2834d4543a46c8c0e25eef230b1262daf26a
liukangtian <liukangtian@topnet.net.cn>
2025-03-19 15:08:10 +0800

    feat: Inject BRANCH_NAME into environment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const { themes } = require('prism-react-renderer');
 const { languageTabs } = require('./static/languageTabs.mjs');
-const isDev = process.env.NODE_ENV === 'development';
+const BRANCH_NAME = process.env.BRANCH_NAME;
+const ALGOLIA_INDEX_NAME = BRANCH_NAME === 'main' ? 'doc_babylonlabs_io' : 'doc_dev_babylonlabs_io';
 const code_themes = {
   light: themes.github,
   dark: themes.dracula,
@@ -290,7 +290,7 @@ const config = {
       algolia: {
         appId: process.env.ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_API_KEY,
-        indexName:  isDev ? 'doc_dev_babylonlabs_io' : 'doc_babylonlabs_io',
+        indexName:  ALGOLIA_INDEX_NAME,
         contextualSearch: true,
         searchParameters: {},
         contextualSearchFilters: [],
